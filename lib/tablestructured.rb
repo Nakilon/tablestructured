e7d40c748a08974193532abe345c2435fd9e28c2
Victor Maslov <nakilon@gmail.com>
2022-07-21 21:02:41 +0300

    ability to drop some leading columns -- they may have no title, such as a column of checkboxes

--- lib/tablestructured.rb
+++ lib/tablestructured.rb
@@ -1,9 +1,20 @@
 module TableStructured
-  def self.new object, headers: :top  # TODO: headers_left should probably mean the ids of entries
+  def self.new object, headers: :top, drop: 0  # TODO: headers_left should probably mean the ids of entries
     if object.respond_to? :css
       # the xml object ignores the :headers arg for now
-      struct = Struct.new *object.at_css("thead").css("th").map(&:text).map(&:to_sym).tap{ |_| fail "headers should be unique" if _.uniq! }
-      object.css("tr").drop(1).map{ |_| struct.new *_.css("td") }
+      ss = if :top == headers
+        object
+      elsif headers.respond_to? :css
+        headers
+      else
+        fail "invalid type of headers"
+      end.css("th").drop(drop).map{ |_| _.text.sub(/\A[[[:space:]]]*/,"").sub(/[[[:space:]]]*\z/,"").to_sym }
+      struct = begin
+        Struct.new *ss
+      rescue NameError
+        raise $!.exception "#{$!}: #{ss.inspect}"
+      end
+      object.css("tbody>tr").map{ |_| struct.new *_.css("td").drop(drop) }
     else
       struct = Struct.new *object.first.map(&:to_s).map(&:to_sym).tap{ |_| fail "headers should be unique" if _.uniq! }
       object.drop(1).map{ |_| struct.new *_ }
